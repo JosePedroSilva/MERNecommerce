5ed5a08250dfe833ddb81a1e10a11b6cae2225ac
Jose Pedro <josepedro88@live.com.pt>
2021-01-14 10:47:02 +0000

    Produdcts details screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+// ROUTER
+import { LinkContainer } from "react-router-bootstrap";
 // STYLES
 import { Navbar, Nav, Container } from "react-bootstrap";
 
@@ -7,16 +9,22 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
         <Container>
-          <Navbar.Brand href="/">eCommerce</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>eCommerce</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i>Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
