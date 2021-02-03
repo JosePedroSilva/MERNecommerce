fc42b34d441badba16d16e50ad7a818191df7615
Jose Pedro <josepedro88@live.com.pt>
2021-02-03 15:50:30 +0000

    user login page and logout handler

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,6 +2,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 
 import axios from "axios";
@@ -30,9 +31,14 @@ export const login = (email, password) => async (dispatch) => {
     dispatch({
       type: USER_LOGIN_FAIL,
       payload:
-        error.response && error.data.message
+        error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
